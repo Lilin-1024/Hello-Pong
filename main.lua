fa27abe0f0bca1fb7e1cc30dbc3d36197578674b
Lilin <LingeonU@gmail.com>
2025-11-25 22:42:18 +0800

    Pong6+7

--- main.lua
+++ main.lua
@@ -44,6 +44,24 @@ end
 
 function love.update(dt)
 
+    if ball:collides(paddle1) then
+        ball.dx = -ball.dx
+    end
+
+    if ball:collides(paddle2) then
+        ball.dx = -ball.dx
+    end
+
+    if ball.y <= 0 then
+        ball.dy = -ball.dy
+        ball.y = 0
+    end
+    
+    if ball.y >= VIRTUAL_HEIGHT - 4 then
+        ball.dy = -ball.dy
+        ball.y = VIRTUAL_HEIGHT - 4
+    end
+
     paddle1:update(dt)
     paddle2:update(dt)
 
@@ -109,5 +127,14 @@ function love.draw()
 
     ball:render()
 
+    displayFPS()
+
     push:apply("end")
+end
+
+function displayFPS()
+    love.graphics.setColor(0,1,0,1)
+    love.graphics.setFont(smallFont)
+    love.graphics.print('FPS: '..tostring(love.timer.getFPS()),40,20)
+    love.graphics.setColor(1,1,1,1)
 end
